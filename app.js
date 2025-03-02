5abedb0519fb5851adb646b6191eafd0d337d1d7
Faniry1015 <frandriaharimino@yahoo.com>
2025-03-02 08:55:01 +0300

    update

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 import { createReadStream, createWriteStream } from 'fs';
+import { writeFile } from 'fs/promises';
 import { createServer } from 'http';
 import { json, text } from 'stream/consumers';
 
 const server = createServer(async (req, res) => {
   const todos = createReadStream('./data/todos.json');
-  // const todosWrite = createWriteStream('./data/todos.json');
-  const todosJson = await json(await text(todos));
+  const todosObj = await json(await text(todos));
   const reqMethod = req.method;
 
   if (reqMethod === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(todosJson));
+    res.write(JSON.stringify(todosObj));
   } else if (reqMethod === 'POST') {
     const body = await json(req);
     const updatedTodo = {
-      id: todosJson.length + 1, ...body, completed: false
+      id: todosObj.length + 1,
+      ...body,
+      completed: false,
     };
-    res.write(JSON.stringify(updatedTodo))
+    const updatedTodos = todosObj.push(updatedTodo);
+    await writeFile('./data/todos.json', JSON.stringify(updatedTodos));
   }
 
   res.end();
