60e6a3b353416589bedf543214063ecd828c49ce
Faniry1015 <frandriaharimino@yahoo.com>
2025-03-03 08:07:51 +0300

    update

--- app.js
+++ app.js
@@ -4,30 +4,31 @@ import { json, text } from 'stream/consumers';
 import { deleteTodo, getTodos, postTodo, putTodo } from './functions/api.js';
 
 const server = createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
   const todos = createReadStream('./data/todos.json');
   const todosObj = await json(await text(todos));
   const reqMethod = req.method;
-  const myURL = new URL(req.url, 'http://localhost:8888');
+  const myURL = new URL(req.url, `http://${req.headers.host}`);
   const id = myURL.searchParams.get('id');
-
-  // Switch case
-  switch (reqMethod) {
-    case 'GET':
-      getTodos(res, todosObj);
-      break;
-    case 'POST':
-      await postTodo(req, res, todosObj);
-      break;
-    case 'DELETE':
-      deleteTodo(res, todosObj, id);
-      break;
-    case 'PUT':
-      putTodo(req, res, todosObj, id);
-      break;
-    default:
-      return;
+  if (url.pathname === '/todos') {
+    // Switch case
+    switch (reqMethod) {
+      case 'GET':
+        getTodos(res, todosObj);
+        break;
+      case 'POST':
+        await postTodo(req, res, todosObj);
+        break;
+      case 'DELETE':
+        deleteTodo(res, todosObj, id);
+        break;
+      case 'PUT':
+        putTodo(req, res, todosObj, id);
+        break;
+      default:
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+    }
   }
-
   res.end();
 });
 
