62fb9af6ca4cf68265bf082fe5aa73e035675530
Faniry1015 <frandriaharimino@yahoo.com>
2025-03-04 17:01:56 +0300

    Ajout de node_modules à .gitignore

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 import fastifyView from "@fastify/view"
 import fastify from "fastify"
 import ejs from 'ejs'
+import fastifyStatic from '@fastify/static'
+import {dirname, join} from 'node:path'
+import {fileURLToPath} from 'node:url'
 
 const app = fastify()
+
+const rootDir = dirname(dirname(fileURLToPath(import.meta.url)))
+console.log(rootDir)
 
 app.register(fastifyView, {
     engine: {
         ejs
     }
 })
+
+app.register(fastifyStatic, {
+    root: join(rootDir, 'public'),
+  })
 
 app.get('/', (req, res) => {
     const posts = [
@@ -28,6 +38,7 @@ app.get('/', (req, res) => {
 const start = async () => {
     try {
         await app.listen({port: 3000})
+        console.log('Server is running on http://localhost:3000')
     } catch (err) {
         console.error(err)
         process.exit(1)
