7ee74a2dbaadf51af3f5efb8318e1ee0e5f9917b
Faniry1015 <frandriaharimino@yahoo.com>
2025-03-15 07:37:30 +0300

    update

--- fastify_try/src/server.js
+++ fastify_try/src/server.js
@@ -1,23 +1,31 @@
-import fastifyView from '@fastify/view';
 import fastify from 'fastify';
+import fastifyView from '@fastify/view';
 import ejs from 'ejs';
+import fastifyStatic from '@fastify/static';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
 const app = fastify();
 
-app.get('/', async (req, res) => {
-  res.view('templates/index.ejs');
-});
+const root_dir = dirname(dirname(fileURLToPath(import.meta.url)))
 
 app.register(fastifyView, {
   engine: {
     ejs,
-  },
+  },  
+});
+
+app.register(fastifyStatic, {
+  root: join(root_dir, 'public'),
+})
+
+app.get('/', async (req, res) => {
+  return res.view('templates/index.ejs');
 });
 
 const start = async () => {
   try {
     app.listen({ port: 3000 });
-    console.log('serveur lancé');
   } catch (e) {
     app.log.error(e);
     process.exit(1);
