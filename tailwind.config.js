1e6ea7bad7e6fc1a10092f9f57f663a0b300772c
Faniry1015 <frandriaharimino@yahoo.com>
2025-03-28 11:22:51 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
-export const content = ['./views/**/*.ejs'];
+export const content = [
+    './templates/**/*.ejs', // Inclut tous les fichiers EJS dans le dossier templates
+    './public/shared/**/*.css', // Inclut tous les fichiers CSS dans le dossier public
+];
 export const theme = {
-    extend: {},
+    extend: {
+        // Ajoutez ici vos personnalisations si nécessaire
+    },
 };
 export const plugins = [];
